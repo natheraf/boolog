55cff868cf57e393ba627217d73a1eb306c86bb7
natheraf <ma.eric11214@outlook.com>
2024-07-01 13:30:59 -0400

    node server written

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const corsOptions = { origin: "http://localhost:3000", credentials: true };
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/jsdoc-front", express.static("../client/jsdoc/out"));
+app.use("/jsdoc-back", express.static("../server/jsdoc/out"));
+app.use(express.static("../client/build"));
+
+const port = process.env.PORT || 8080;
+
+/**
+ * If `dev` was in our arguments, we will start the app by listening for HTTP requests. This is used for development.
+ *
+ * Otherwise we will start our app in production mode with SSL certificates.
+ */
+if (process.argv.includes("dev")) {
+  app.listen(port, () => console.log(`Express Server on port ${port}`));
+} else {
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+  const fs = require("fs");
+  const https = require("https");
+  const httpServer = https.createServer(
+    {
+      key: fs.readFileSync(process.env.PRIVATEKEY, "utf-8"),
+      cert: fs.readFileSync(process.env.CERTIFICATE, "utf-8"),
+    },
+    app
+  );
+
+  httpServer.listen(port, () => console.log(`Express Server on port ${port}`));
+}
