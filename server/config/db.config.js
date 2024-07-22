c9be945b4f4bbf5eb7f34cf37e2a79677a995095
natheraf <ma.eric11214@outlook.com>
2024-07-22 19:17:45 -0400

    db config

---
+++ server/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  db: "postgres",
+  dialect: "postgres",
+  pool: {
+    max: 5, // maximum number of connection in pool
+    min: 0, // minimum number of connection in pool
+    acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
+    idle: 10000, // maximum time, in milliseconds, that a connection can be idle before being released
+  },
+};
