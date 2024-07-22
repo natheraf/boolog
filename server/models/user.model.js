87b8830985108556738bb0f639255f44ad320ac6
natheraf <ma.eric11214@outlook.com>
2024-07-22 19:18:20 -0400

    user and role seq models

---
+++ server/models/user.model.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+  });
+
+  return User;
+};
