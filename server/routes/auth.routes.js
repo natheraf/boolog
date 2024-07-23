51ec9964f72f0db0dba660715a69cb6e5151633c
natheraf <ma.eric11214@outlook.com>
2024-07-23 02:00:50 -0400

    created route for authentication

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,27 @@
+const { authJwt, verifySignUp } = require("../middleware");
+const controller = require("../controller/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/signup",
+    [
+      authJwt.verifyToken,
+      verifySignUp.checkRolesExist,
+      authJwt.authorizedToCreateUser,
+      verifySignUp.checkDuplicateNameOrEmail,
+    ],
+    controller.signUp
+  );
+
+  app.post("/api/auth/signin", controller.signIn);
+
+  app.post("/api/auth/signout", controller.signOut);
+};
