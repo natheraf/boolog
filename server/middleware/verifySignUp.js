773c8a5237c601f856b567c0bf3e7501b6f32235
natheraf <ma.eric11214@outlook.com>
2024-07-23 02:00:35 -0400

    added function to check duplicate email or name and check if role exist when creating user

---
+++ server/middleware/verifySignUp.js
@@ -0,0 +1,52 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+const Op = db.Sequelize.Op;
+
+const checkDuplicateNameOrEmail = (req, res, next) => {
+  const missing = ["name", "email"].filter((key) => !req.body[key]);
+  if (missing.length > 0) {
+    res.status(400).send({
+      message: "User's " + missing.join(" and ") + " cannot be empty",
+    });
+    return;
+  }
+
+  User.findOne({
+    where: {
+      [Op.or]: {
+        name: req.body.name,
+        email: req.body.email,
+      },
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({
+        message: "Name is already in use!",
+      });
+      return;
+    }
+    next();
+  });
+};
+
+const checkRolesExist = (req, res, next) => {
+  if (
+    !req.body.role ||
+    req.body.role === "super_admin" ||
+    !ROLES.includes(req.body.role)
+  ) {
+    res.status(400).send({
+      message: "Blank role or role does not exit",
+    });
+    return;
+  }
+  next();
+};
+
+const verifySignUp = {
+  checkDuplicateNameOrEmail,
+  checkRolesExist,
+};
+
+module.exports = verifySignUp;
