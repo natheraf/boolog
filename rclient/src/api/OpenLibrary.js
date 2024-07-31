32ae701cf443cd92c7524ec1904c338937350517
natheraf <ma.eric11214@outlook.com>
2024-07-31 02:53:08 -0400

    openlibrary API

---
+++ rclient/src/api/OpenLibrary.js
@@ -0,0 +1,11 @@
+export const searchOpenLib = (query) => {
+  const url = `https://openlibrary.org/search.json?q=${query}&limit=5`;
+  const options = {
+    method: "GET",
+  };
+  return new Promise((resolve, reject) =>
+    fetch(url, options)
+      .then((response) => resolve(response.json()))
+      .catch((error) => reject(Error(error)))
+  );
+};
