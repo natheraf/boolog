f5cfb3b9d3d08f1f062c0ea75a04e46d3e63d4a1
natheraf <ma.eric11214@outlook.com>
2024-08-06 00:37:15 -0400

    added api key for google books

--- rclient/src/api/GoogleAPI.js
+++ rclient/src/api/GoogleAPI.js
@@ -1,7 +1,7 @@
 export const searchGoogleBooks = (query, pageLimit = 5, page = 1) => {
   const url = `https://www.googleapis.com/books/v1/volumes?${query}&maxResults=${pageLimit}&startIndex=${
     page - 1
-  }`;
+  }&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`;
   const options = {
     method: "GET",
   };
