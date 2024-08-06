fc057777c2d3b965f9a9b78852de709cbee396ff
natheraf <ma.eric11214@outlook.com>
2024-08-06 02:36:24 -0400

    added theme

--- rclient/src/App.js
+++ rclient/src/App.js
@@ -4,20 +4,62 @@ import * as React from "react";
 
 import { Login } from "./views/Login";
 import { SearchBook } from "./views/SearchBook";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+export const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
 
 function App() {
+  const [mode, setMode] = React.useState(
+    localStorage.getItem("darkMode") === undefined ||
+      localStorage.getItem("darkMode") === "light"
+      ? "light"
+      : "dark"
+  );
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => {
+          localStorage.setItem(
+            "darkMode",
+            prevMode === "light" ? "dark" : "light"
+          );
+          return prevMode === "light" ? "dark" : "light";
+        });
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: "dark",
+        },
+      }),
+    []
+  );
+
   return (
-    <Router>
-      <Container maxWidth="xl" sx={{ mt: "88px" }}>
-        <Routes>
-          <Route path="" element={<h1>Home</h1>} />
-          <Route path="*" element={<h1>Wrong path</h1>} />
-          <Route path="login" element={<Login />} />
-          <Route path="loggedIn" element={<h1>Logged In</h1>} />
-          <Route path="searchBook" element={<SearchBook />} />
-        </Routes>
-      </Container>
-    </Router>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Container maxWidth="xl" sx={{ mt: "88px" }}>
+            <Routes>
+              <Route path="" element={<h1>Home</h1>} />
+              <Route path="*" element={<h1>Wrong path</h1>} />
+              <Route path="login" element={<Login />} />
+              <Route path="loggedIn" element={<h1>Logged In</h1>} />
+              <Route path="searchBook" element={<SearchBook />} />
+            </Routes>
+          </Container>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
