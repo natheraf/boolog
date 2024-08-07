bc591728eb7bb996def4f9ab9d1a56ea9c6a80ce
natheraf <ma.eric11214@outlook.com>
2024-08-06 20:00:47 -0400

    header responsive

--- rclient/src/components/Header.js
+++ rclient/src/components/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, Stack, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Grid, Stack, Toolbar, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Header = () => {
@@ -11,40 +11,45 @@ export const Header = () => {
         top: 0,
       }}
     >
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Stack direction="inherit" alignItems={"inherit"} gap={5}>
-          <Box
-            component="img"
-            src={require("../assets/logo_remix0-trimmed.png")}
-            alt={`logo`}
-            sx={{
-              borderRadius: "5px",
-              display: "block",
-              width: "auto",
-              height: "auto",
-              maxWidth: "50px",
-              maxHeight: "50px",
-            }}
-            onClick={() => navigate("/")}
-          />
-          <Link to={"/"} style={{ textDecoration: "none", color: "white" }}>
-            <Typography variant="h6">Library</Typography>
-          </Link>
-          <Link
-            to={"/searchBook"}
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <Typography variant="h6">Search</Typography>
-          </Link>
-        </Stack>
-        <Stack>
-          <Link
-            to={"/login"}
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <Typography variant="h6">Login</Typography>
-          </Link>
-        </Stack>
+      <Toolbar>
+        <Grid
+          container
+          justifyContent={{ xs: "space-around", sm: "space-between" }}
+        >
+          <Stack direction="row" alignItems={"center"} spacing={5} pr={5}>
+            <Box
+              component="img"
+              src={require("../assets/logo_remix0-trimmed.png")}
+              alt={`logo`}
+              sx={{
+                borderRadius: "5px",
+                display: "block",
+                width: "auto",
+                height: "auto",
+                maxWidth: "50px",
+                maxHeight: "50px",
+              }}
+              onClick={() => navigate("/")}
+            />
+            <Link to={"/"} style={{ textDecoration: "none", color: "white" }}>
+              <Typography variant="h6">Library</Typography>
+            </Link>
+            <Link
+              to={"/searchBook"}
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <Typography variant="h6">Search</Typography>
+            </Link>
+          </Stack>
+          <Stack direction="row" alignItems={"center"}>
+            <Link
+              to={"/login"}
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <Typography variant="h6">Login</Typography>
+            </Link>
+          </Stack>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
