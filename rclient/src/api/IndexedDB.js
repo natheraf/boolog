5e8a45d1e118d6028172ddc2fa103561c06f5f30
natheraf <ma.eric11214@outlook.com>
2024-08-19 03:11:14 -0400

    refactor to always open before transaction and close after

--- rclient/src/api/IndexedDB.js
+++ rclient/src/api/IndexedDB.js
@@ -1,24 +1,19 @@
-const userDataDB = indexedDB.open("userData", 1);
+const userDataDB = "userData";
+const userDataDBVersion = 1;
 
-userDataDB.onerror = function () {
-  console.error("Error", userDataDB.error);
+const connect = (name, version) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(name, version);
+    request.onupgradeneeded = userDataDBOnupgradeneeded;
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+    request.onblocked = () =>
+      alert("Database is outdated, please reload the page.");
+  });
 };
 
-userDataDB.onsuccess = function () {
-  const db = userDataDB.result;
-  db.onversionchange = function () {
-    db.close();
-    alert("Database is outdated, please reload the page.");
-  };
-  // continue working with database using db object
-};
-
-userDataDB.onblocked = function () {
-  alert("Database is outdated, please reload the page.");
-};
-
-userDataDB.onupgradeneeded = function (event) {
-  const db = userDataDB.result;
+const userDataDBOnupgradeneeded = function (event) {
+  const db = event.target.result;
   switch (event.oldVersion) {
     case 0:
       var userBooks = db.createObjectStore("books", {
@@ -52,92 +47,132 @@ const test = () => {
   });
 };
 
+export const openDatabase = async (name, version, crudFn) => {
+  let db;
+  try {
+    db = await connect(userDataDB, userDataDBVersion);
+    await crudFn(db);
+  } finally {
+    if (db) {
+      db.close();
+    }
+  }
+};
+
 export const addBook = (obj) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readwrite");
-  const objectStore = transaction.objectStore("books");
-  const request = objectStore.add(obj);
-  request.onsuccess = () =>
-    console.log("book add request completed successfully");
-  request.onerror = () => console.log("Request Error", request.error);
+  openDatabase(userDataDB, userDataDBVersion, (db) => addBookHelper(db, obj));
 };
 
-export const getAllBooks = (obj) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readonly");
-  transaction.oncomplete = (event) => {
-    console.log("got all books successfully");
-  };
-  transaction.onerror = (event) => {
-    console.error("Transaction Error", event);
-  };
-  const objectStore = transaction.objectStore("books");
-  objectStore.getAll().onsuccess = (event) => {
-    return event.target.result;
-  };
+const addBookHelper = (db, obj) =>
+  new Promise((resolve, reject) => {
+    const transaction = db.transaction("books", "readwrite");
+    const objectStore = transaction.objectStore("books");
+    const request = objectStore.add(obj);
+    request.onsuccess = () =>
+      console.log("book add request completed successfully");
+    request.onerror = () => console.log("Request Error", request.error);
+  });
+
+export const getAllBooks = (callback) => {
+  openDatabase(userDataDB, userDataDBVersion, (db) =>
+    getAllBooksHelper(db, callback)
+  );
 };
 
-export const setBookStatus = (obj) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readwrite");
-  const objectStore = transaction.objectStore("books");
-  const index = objectStore.index("isbn");
-  obj.isbn.forEach((isbn) => {
-    const request = index.get(isbn);
-    request.onsuccess = (event) => {
-      const data = event.target.result;
-      if (data !== undefined) {
-        data.status = obj.status;
-        const requestUpdate = objectStore.put(data);
-        requestUpdate.onsuccess = () => console.log("updated book status");
-        requestUpdate.onerror = () =>
-          console.log("update book status error", requestUpdate.error);
-      } else {
-        addBook(obj);
-      }
+const getAllBooksHelper = (db, callback) =>
+  new Promise((resolve, reject) => {
+    const transaction = db.transaction("books", "readonly");
+    transaction.oncomplete = (event) => {
+      console.log("got all books successfully");
+    };
+    transaction.onerror = (event) => {
+      console.error("Transaction Error", event);
+    };
+    const objectStore = transaction.objectStore("books");
+    objectStore.getAll().onsuccess = (event) => {
+      callback(event.target.result);
     };
   });
+
+export const setBookStatus = (obj) => {
+  openDatabase(userDataDB, userDataDBVersion, (db) =>
+    setBookStatusHelper(db, obj)
+  );
 };
+
+const setBookStatusHelper = (db, obj) =>
+  new Promise((resolve, reject) => {
+    const transaction = db.transaction("books", "readwrite");
+    const objectStore = transaction.objectStore("books");
+    const index = objectStore.index("isbn");
+    obj.isbn.forEach((isbn) => {
+      const request = index.get(isbn);
+      request.onsuccess = (event) => {
+        const data = event.target.result;
+        if (data !== undefined) {
+          data.status = obj.status;
+          const requestUpdate = objectStore.put(data);
+          requestUpdate.onsuccess = () => console.log("updated book status");
+          requestUpdate.onerror = () =>
+            console.log("update book status error", requestUpdate.error);
+        } else {
+          addBook(obj);
+        }
+      };
+    });
+  });
 
 export const getBookStatus = (obj, setStatus) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readwrite");
-  const objectStore = transaction.objectStore("books");
-  const index = objectStore.index("isbn");
-  if (obj.isbn !== undefined) {
-    obj.isbn.forEach((isbn) => {
-      const request = index.get(isbn);
-      request.onsuccess = (event) => {
-        const data = event.target.result;
-        if (data !== undefined) {
-          setStatus(data.status);
-        }
-      };
-    });
-  } else {
-    // isbn not found
-  }
+  openDatabase(userDataDB, userDataDBVersion, (db) =>
+    getBookStatusHelper(db, obj, setStatus)
+  );
 };
 
+const getBookStatusHelper = (db, obj, setStatus) =>
+  new Promise((resolve, reject) => {
+    const transaction = db.transaction("books", "readwrite");
+    const objectStore = transaction.objectStore("books");
+    const index = objectStore.index("isbn");
+    if (obj.isbn !== undefined) {
+      obj.isbn.forEach((isbn) => {
+        const request = index.get(isbn);
+        request.onsuccess = (event) => {
+          const data = event.target.result;
+          if (data !== undefined) {
+            setStatus(data.status);
+          }
+        };
+      });
+    } else {
+      // isbn not found
+    }
+  });
+
 export const deleteBook = (obj) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readwrite");
-  const objectStore = transaction.objectStore("books");
-  const index = objectStore.index("isbn");
-  if (obj.isbn !== undefined) {
-    obj.isbn.forEach((isbn) => {
-      const request = index.get(isbn);
-      request.onsuccess = (event) => {
-        const data = event.target.result;
-        if (data !== undefined) {
-          objectStore.delete(data.id);
-        }
-      };
-    });
-  } else {
-    // isbn not found
-  }
+  openDatabase(userDataDB, userDataDBVersion, (db) =>
+    deleteBookHelper(db, obj)
+  );
 };
+
+const deleteBookHelper = (db, obj) =>
+  new Promise((resolve, reject) => {
+    const transaction = db.transaction("books", "readwrite");
+    const objectStore = transaction.objectStore("books");
+    const index = objectStore.index("isbn");
+    if (obj.isbn !== undefined) {
+      obj.isbn.forEach((isbn) => {
+        const request = index.get(isbn);
+        request.onsuccess = (event) => {
+          const data = event.target.result;
+          if (data !== undefined) {
+            objectStore.delete(data.id);
+          }
+        };
+      });
+    } else {
+      // isbn not found
+    }
+  });
 
 export const indexedDBBooksInterface = {
   addBook,
@@ -147,66 +182,66 @@ export const indexedDBBooksInterface = {
   deleteBook,
 };
 
-export const getBook = (id) => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readonly");
-  transaction.oncomplete = (event) => {
-    console.log("book get successfully");
-  };
-  transaction.onerror = (event) => {
-    console.error("Transaction Error", event);
-  };
-  const objectStore = transaction.objectStore("books");
-  const request = objectStore.get(id);
-  request.onsuccess = () =>
-    console.log("book get request completed successfully", request.result);
-  request.onerror = () => console.log("Request Error", request.error);
-};
+// export const getBook = (id) => {
+//   const db = userDataDB.result;
+//   const transaction = db.transaction("books", "readonly");
+//   transaction.oncomplete = (event) => {
+//     console.log("book get successfully");
+//   };
+//   transaction.onerror = (event) => {
+//     console.error("Transaction Error", event);
+//   };
+//   const objectStore = transaction.objectStore("books");
+//   const request = objectStore.get(id);
+//   request.onsuccess = () =>
+//     console.log("book get request completed successfully", request.result);
+//   request.onerror = () => console.log("Request Error", request.error);
+// };
 
-export const getTitles = () => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readonly");
-  transaction.oncomplete = (event) => {
-    console.log("book get successfully");
-  };
-  transaction.onerror = (event) => {
-    console.error("Transaction Error", event);
-  };
-  const objectStore = transaction.objectStore("books");
-  // const index = objectStore.index("title");
-  // index.openCursor().onsuccess = (event) => {
-  //   const cursor = event.target.result;
-  //   if (cursor) {
-  //     // cursor.key is a name, like "Bill", and cursor.value is the whole object.
-  //     console.log(cursor.value);
-  //     cursor.continue();
-  //   }
-  // };
-  objectStore.getAll().onsuccess = (event) => {
-    console.log(event.target.result);
-  };
-  // request.onsuccess = () =>
-  //   console.log("book get request completed successfully", request.result);
-  // request.onerror = () => console.log("Request Error", request.error);
-};
+// export const getTitles = () => {
+//   const db = userDataDB.result;
+//   const transaction = db.transaction("books", "readonly");
+//   transaction.oncomplete = (event) => {
+//     console.log("book get successfully");
+//   };
+//   transaction.onerror = (event) => {
+//     console.error("Transaction Error", event);
+//   };
+//   const objectStore = transaction.objectStore("books");
+//   // const index = objectStore.index("title");
+//   // index.openCursor().onsuccess = (event) => {
+//   //   const cursor = event.target.result;
+//   //   if (cursor) {
+//   //     // cursor.key is a name, like "Bill", and cursor.value is the whole object.
+//   //     console.log(cursor.value);
+//   //     cursor.continue();
+//   //   }
+//   // };
+//   objectStore.getAll().onsuccess = (event) => {
+//     console.log(event.target.result);
+//   };
+//   // request.onsuccess = () =>
+//   //   console.log("book get request completed successfully", request.result);
+//   // request.onerror = () => console.log("Request Error", request.error);
+// };
 
-export const getInteresting = () => {
-  const db = userDataDB.result;
-  const transaction = db.transaction("books", "readonly");
-  transaction.oncomplete = (event) => {
-    console.log("book get successfully");
-  };
-  transaction.onerror = (event) => {
-    console.error("Transaction Error", event);
-  };
-  const objectStore = transaction.objectStore("books");
-  const index = objectStore.index("interesting");
-  index.openCursor().onsuccess = (event) => {
-    const cursor = event.target.result;
-    if (cursor) {
-      // cursor.key is a name, like "Bill", and cursor.value is the whole object.
-      console.log(cursor.value);
-      cursor.continue();
-    }
-  };
-};
+// export const getInteresting = () => {
+//   const db = userDataDB.result;
+//   const transaction = db.transaction("books", "readonly");
+//   transaction.oncomplete = (event) => {
+//     console.log("book get successfully");
+//   };
+//   transaction.onerror = (event) => {
+//     console.error("Transaction Error", event);
+//   };
+//   const objectStore = transaction.objectStore("books");
+//   const index = objectStore.index("interesting");
+//   index.openCursor().onsuccess = (event) => {
+//     const cursor = event.target.result;
+//     if (cursor) {
+//       // cursor.key is a name, like "Bill", and cursor.value is the whole object.
+//       console.log(cursor.value);
+//       cursor.continue();
+//     }
+//   };
+// };
