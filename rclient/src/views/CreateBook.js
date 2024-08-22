c8a60568fcafe6ed87d42c1de0f38d0cb5423dd3
natheraf <ma.eric11214@outlook.com>
2024-08-22 02:35:38 -0400

    add books feature

--- rclient/src/views/CreateBook.js
+++ rclient/src/views/CreateBook.js
@@ -13,6 +13,7 @@ import {
   ToggleButton,
   ToggleButtonGroup,
   Toolbar,
+  Tooltip,
   Typography,
   useMediaQuery,
 } from "@mui/material";
@@ -20,6 +21,12 @@ import CloseIcon from "@mui/icons-material/Close";
 import SaveIcon from "@mui/icons-material/Save";
 import { useTheme } from "@emotion/react";
 import { Textarea } from "../components/Textarea";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import StopIcon from "@mui/icons-material/Stop";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import DoneIcon from "@mui/icons-material/Done";
+import { addBook } from "../api/IndexedDB";
 
 const DialogSlideUpTransition = React.forwardRef(function Transition(
   props,
@@ -31,76 +38,118 @@ const DialogSlideUpTransition = React.forwardRef(function Transition(
 export const CreateBook = ({ open, setOpen }) => {
   const theme = useTheme();
   const greaterThanSmall = useMediaQuery(theme.breakpoints.up("sm"));
-  const [bookType, setBookType] = React.useState(null);
+  const greaterThanMedium = useMediaQuery(theme.breakpoints.up("md"));
+  const [bookObject, setBookObject] = React.useState({});
 
   const handleClose = () => {
     setOpen(false);
-    setBookType(null);
+    setBookObject((prev) => ({ ...prev, status: null, type: null }));
   };
 
-  const handleOnChangeBookType = (event, value) => {
-    if (value !== null) setBookType(value);
+  const handleSave = () => {
+    const keysToProcessMultiple = [
+      "isbn",
+      "authors",
+      "genres_subjects",
+      "publisher",
+    ];
+    keysToProcessMultiple.forEach(
+      (key) =>
+        (bookObject[key] = bookObject[key]
+          ?.split(",")
+          .map((isbn) => isbn.trim()))
+    );
+    addBook(bookObject)
+      .then((res) => handleClose())
+      .catch((error) => console.log(error));
+  };
+
+  const handleOnChangeProperty = (key) => (event, value) => {
+    if (value !== null) {
+      setBookObject((prev) => ({
+        ...prev,
+        [key]: event?.target?.value ?? value,
+      }));
+    }
   };
 
   const bookKeys = () => {
-    const keys = [
-      { label: "Title", key: "title", width: "40%", priority: 0 },
-      { label: "Authors", key: "authors", width: "40%", priority: 1 },
-      { label: "Description", key: "description", width: "40%", priority: 4 },
-      {
-        label: "Genres",
-        key: "genres_subjects",
-        width: "20%",
-        priority: 8,
-      },
-      {
-        label: "Year",
-        key: "publish_year",
-        width: "10%",
-        priority: 7,
-      },
-      {
-        label: "Number of Pages",
-        key: "number_of_pages",
-        width: "10%",
-        priority: 10,
-      },
-      {
-        label: "Number of Chapters",
-        key: "number_of_chapters",
-        width: "10%",
-        priority: 11,
-      },
-      {
-        label: "Number of Words",
-        key: "number_of_words",
-        width: "10%",
-        priority: 12,
-      },
-    ];
-    if (["novel", "light novel/manga"].includes(bookType)) {
-      keys.push(
-        { label: "Publishers", key: "publisher", width: "40%", priority: 2 },
-        { label: "ISBNs", key: "isbn", width: "20%", priority: 3 },
+    const keys = {
+      common: [
+        { label: "Title", key: "title", width: "40%", priority: 0 },
         {
-          label: "Links to Purchase",
-          key: "purchase_links",
+          label: "Authors",
+          key: "authors",
+          width: "40%",
+          priority: 1,
+          placeholder: "Eiji Mikage, Miya Kazuki, Isuna Hasekura",
+        },
+        { label: "Description", key: "description", width: "40%", priority: 4 },
+        {
+          label: "Genres / Subjects",
+          key: "genres_subjects",
           width: "20%",
-          priority: 14,
-        }
-      );
-    } else if (["web novel", "webtoon"].includes(bookType)) {
-      keys.push(
+          priority: 8,
+          placeholder: "Action, Romance, Mystery, Thriller",
+        },
+        {
+          label: "Year Released",
+          key: "publish_year",
+          width: "10%",
+          priority: 7,
+        },
+        {
+          label: "Number of Pages",
+          key: "number_of_pages",
+          width: "10%",
+          priority: 10,
+        },
+        {
+          label: "Number of Chapters",
+          key: "number_of_chapters",
+          width: "10%",
+          priority: 11,
+        },
+        {
+          label: "Number of Words",
+          key: "number_of_words",
+          width: "10%",
+          priority: 12,
+        },
+      ],
+      unique: [],
+    };
+    if (["novel", "light novel/manga"].includes(bookObject.type)) {
+      keys.unique = [
+        {
+          label: "Publishers",
+          key: "publisher",
+          width: "40%",
+          priority: 2,
+          placeholder: "Yen Press, Yen On, J-Novel Club",
+        },
+        {
+          label: "ISBNs",
+          key: "isbn",
+          width: "40%",
+          priority: 3,
+          placeholder: "9780316561105, 978-0316561105, 9780316561235",
+        },
+      ];
+    } else if (["web novel", "webtoon"].includes(bookObject.type)) {
+      keys.unique = [
         {
           label: "Release Schedule",
           key: "release_schedule",
           width: "10%",
           priority: 9,
         },
-        { label: "Views", key: "views", width: "10%", priority: 13 }
-      );
+        { label: "Views", key: "views", width: "10%", priority: 13 },
+      ];
     }
-    keys.sort((a, b) => a.priority - b.priority);
+    Object.values(keys).forEach((array) =>
+      array.toSorted((a, b) => a.priority - b.priority)
+    );
     return keys;
   };
 
@@ -129,32 +178,70 @@ export const CreateBook = ({ open, setOpen }) => {
           <Typography variant="h6">Create Entry</Typography>
           <Button
             color="inherit"
-            onClick={handleClose}
+            onClick={handleSave}
             endIcon={<SaveIcon />}
             sx={{ alignItems: "center" }}
+            disabled={
+              !bookObject.type ||
+              !bookObject.status ||
+              !bookObject.title?.length > 0
+            }
           >
             save
           </Button>
         </Toolbar>
       </AppBar>
       <Stack alignItems={"center"} spacing={2} p={2}>
-        <ToggleButtonGroup
-          value={bookType}
-          exclusive
-          onChange={handleOnChangeBookType}
-          aria-label="change book type"
-          orientation={greaterThanSmall ? "horizontal" : "vertical"}
-        >
-          {["novel", "light novel/manga", "web novel", "webtoon"].map(
-            (value) => (
-              <ToggleButton key={value} value={value} aria-label={value}>
-                {value}
-              </ToggleButton>
-            )
-          )}
-        </ToggleButtonGroup>
-        <Fade in={bookType !== null} mountOnEnter>
+        <Stack spacing={2} direction={greaterThanMedium ? "row" : "column"}>
+          <Stack direction="column" spacing={1}>
+            <Typography variant="body">Type: </Typography>
+            <ToggleButtonGroup
+              value={bookObject.type}
+              exclusive
+              onChange={handleOnChangeProperty("type")}
+              aria-label="change book type"
+              orientation={greaterThanSmall ? "horizontal" : "vertical"}
+            >
+              {["novel", "light novel/manga", "web novel", "webtoon"].map(
+                (value) => (
+                  <ToggleButton key={value} value={value} aria-label={value}>
+                    {value}
+                  </ToggleButton>
+                )
+              )}
+            </ToggleButtonGroup>
+          </Stack>
+          <Stack direction="column" spacing={1}>
+            <Typography variant="body">Status: </Typography>
+            <ToggleButtonGroup
+              value={bookObject.status}
+              exclusive
+              onChange={handleOnChangeProperty("status")}
+              aria-label="change book status"
+            >
+              {[
+                { title: "Read", value: "Reading", icon: PlayArrowIcon },
+                { title: "Pause", value: "Paused", icon: PauseIcon },
+                { title: "Drop", value: "Dropped", icon: StopIcon },
+                {
+                  title: "Plan to Read",
+                  value: "Planning",
+                  icon: PlaylistAddIcon,
+                },
+                { title: "Finish", value: "Finished", icon: DoneIcon },
+              ].map((obj) => (
+                <Tooltip key={obj.value} title={obj.title}>
+                  <ToggleButton value={obj.value} aria-label={obj.value}>
+                    <obj.icon />
+                  </ToggleButton>
+                </Tooltip>
+              ))}
+            </ToggleButtonGroup>
+          </Stack>
+        </Stack>
+        <Fade in={bookObject.type && bookObject.status} mountOnEnter>
           <Stack direction={"column"} spacing={2}>
+            <Divider>Shared Properties</Divider>
             <Grid
               gap={2}
               container
@@ -163,7 +250,7 @@ export const CreateBook = ({ open, setOpen }) => {
                 justifyContent: { xs: "center", sm: "flex-start" },
               }}
             >
-              {bookKeys().map((obj) => (
+              {bookKeys().common.map((obj) => (
                 <Grid item key={obj.key} sx={{ minWidth: obj.width }}>
                   {obj.key === "description" ? (
                     <Textarea
@@ -173,14 +260,46 @@ export const CreateBook = ({ open, setOpen }) => {
                       aria-label={obj.label}
                       placeholder={obj.label}
                       sx={{ width: "100%" }}
+                      value={bookObject[obj.key] ?? ""}
+                      onChange={handleOnChangeProperty(obj.key)}
                     />
                   ) : (
-                    <TextField label={obj.label} fullWidth />
+                    <TextField
+                      label={obj.label}
+                      fullWidth
+                      required={obj.key === "title"}
+                      id={`tf-${obj.key}`}
+                      value={bookObject[obj.key] ?? ""}
+                      onChange={handleOnChangeProperty(obj.key)}
+                      placeholder={obj.placeholder}
+                    />
                   )}
                 </Grid>
               ))}
             </Grid>
-            <Divider />
+            <Divider>Unique Type Properties</Divider>
+            <Grid
+              gap={2}
+              container
+              direction={"row"}
+              sx={{
+                justifyContent: { xs: "center", sm: "flex-start" },
+              }}
+            >
+              {bookKeys().unique.map((obj) => (
+                <Grid item key={obj.key} sx={{ minWidth: obj.width }}>
+                  <TextField
+                    label={obj.label}
+                    fullWidth
+                    id={`tf-${obj.key}`}
+                    value={bookObject[obj.key] ?? ""}
+                    onChange={handleOnChangeProperty(obj.key)}
+                    placeholder={obj.placeholder}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+            <Divider>Links</Divider>
             <Grid
               gap={2}
               container
@@ -193,12 +312,18 @@ export const CreateBook = ({ open, setOpen }) => {
                 {
                   label: "Cover URL",
                   key: "cover_url",
-                  width: "40%",
+                  width: "30%",
                 },
                 {
                   label: "Link to Read",
                   key: "read_link",
-                  width: "40%",
+                  width: "30%",
+                },
+                {
+                  label: "Links to Purchase",
+                  key: "purchase_links",
+                  width: "30%",
+                  priority: 14,
                 },
               ].map((obj) => (
                 <Grid item key={obj.key} sx={{ minWidth: obj.width }}>
