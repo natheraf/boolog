5735195ec7330ddc8a6d6e92a9f2831a386d91ae
natheraf <ma.eric11214@outlook.com>
2024-09-21 19:55:31 -0400

    mailer now hashes login code

--- server/mongo/middleware/mailer.js
+++ server/mongo/middleware/mailer.js
@@ -1,13 +1,15 @@
 require("dotenv").config({ path: __dirname + "/../../.env" });
+const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const config = require("../config/email.config.js");
 
 const generateRandomCode = (numOfBytes) =>
   require("crypto").randomBytes(numOfBytes).toString("base64url");
 
-const emailAuthenticationCheck = (receiverEmail, receiverName) =>
+const emailAuthenticationCheck = (req) =>
   new Promise((resolve, reject) => {
-    const code = generateRandomCode(4);
+    const link = `${req.header("Origin")}/login/passwordless`;
+    const code = generateRandomCode(64);
     const transporter = nodemailer.createTransport({
       host: `${config.outgoing_email_host}`,
       port: 587,
@@ -20,38 +22,42 @@ const emailAuthenticationCheck = (receiverEmail, receiverName) =>
     transporter
       .sendMail({
         from: `"No-Reply Boolog 📖" <${config.outgoing_email}>`, // sender address
-        to: `${receiverEmail}`, // list of receivers
+        to: `${req.body.email}`, // list of receivers
         subject: "Verify Your Identity", // Subject line
         text: `Hi ${
-          receiverName ?? receiverEmail
-        }. This is your verification code: ${code}`, // plain text body
+          req.body.name ?? req.body.email
+        }. This is your verification link: ${link}?code=${code}`, // plain text body
         html: `<p>Hi ${
-          receiverName ?? receiverEmail
-        }<br/><b>One-time verification code</b><br/>Here is your one-time verification code to verify its you. This code is valid for <b>5 minutes</b> only: </p><h1>${code}</h1><p>Thanks, Boolog.</p>`, // html body
+          req.body.name ?? req.body.email
+        }<br/><p><b>One-time verification link</b></p><p>Here is your one-time verification link to verify its you. This link is valid for <b>5 minutes</b> only: </p><a href="${link}?code=${code}&email=${
+          req.body.email
+        }"><h2>Click here to Login</h2></a><p>Thanks, Boolog.</p>`, // html body
       })
       .then((info) => resolve({ code, info }))
       .catch((error) => reject(error));
   });
 
-const sendEmailAuthentication = (db, receiverEmail, receiverName) =>
+const sendEmailAuthentication = (db, req) =>
   new Promise((resolve, reject) => {
-    emailAuthenticationCheck(receiverEmail, receiverName).then(
-      ({ code, info }) =>
-        db
-          .collection("loginEmailCodes")
-          .insertOne({ email: receiverEmail, code, info })
-          .then((insertRes) => {
-            setTimeout(
-              () =>
-                db.collection("loginEmailCodes").deleteOne({
-                  _id: insertRes.insertedId,
-                  email: receiverEmail,
-                }),
-              // 1000 * 60 * 5 // 5 minutes
-              1000 * 60 // 1 minute
-            );
-            resolve("Sent verification code");
-          })
+    emailAuthenticationCheck(req).then(({ code, info }) =>
+      db
+        .collection("loginEmailCodes")
+        .insertOne({
+          email: req.body.email,
+          code: bcrypt.hashSync(code, 10),
+          info,
+        })
+        .then((insertRes) => {
+          setTimeout(
+            () =>
+              db.collection("loginEmailCodes").deleteOne({
+                _id: insertRes.insertedId,
+                email: req.body.email,
+              }),
+            1000 * 60 * 5 // 5 minutes
+          );
+          resolve("Sent verification code");
+        })
     );
   });
 
