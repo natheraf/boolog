5db17c9b1acae5fdd4840cf24f2da4ff1a80be1b
natheraf <ma.eric11214@outlook.com>
2024-09-21 02:14:04 -0400

    fun for checking if email is duplicate

--- server/mongo/middleware/verifySignUp.js
+++ server/mongo/middleware/verifySignUp.js
@@ -1,25 +1,32 @@
 const { getDatabase, ROLES } = require("../database");
+const { bodyMissingRequiredFields } = require("./utils");
+
+const isDuplicateEmail = (email) =>
+  new Promise((resolve, reject) =>
+    getDatabase("authentication")
+      .then((db) => {
+        const collection = db.collection("loginInfo");
+        collection.findOne({ email }).then((email) => {
+          resolve(email !== null);
+        });
+      })
+      .catch(reject)
+  );
 
 const checkDuplicateEmail = (req, res, next) => {
-  const missing = ["email"].filter((key) => !req.body[key]);
-  if (missing.length > 0) {
-    res.status(400).send({
-      message: "User's " + missing.join(" and ") + " cannot be empty",
-    });
-    return;
+  const missing = bodyMissingRequiredFields(req, ["email"]);
+  if (missing) {
+    return res?.status(400).send(missing);
   }
 
-  getDatabase("authentication").then((db) => {
-    const collection = db.collection("loginInfo");
-    collection.findOne({ email: req.body.email }).then((email) => {
-      if (email) {
-        res.status(400).send({
-          message: "Email is already in use!",
-        });
-        return;
-      }
-      next();
-    });
+  isDuplicateEmail(req.body.email).then((isDuplicate) => {
+    if (isDuplicate) {
+      res.status(400).send({
+        message: "Email is already in use!",
+      });
+      return;
+    }
+    next();
   });
 };
 
@@ -34,6 +41,7 @@ const checkRolesExist = (req, res, next) => {
 };
 
 const verifySignUp = {
+  isDuplicateEmail,
   checkDuplicateEmail,
   checkRolesExist,
 };
