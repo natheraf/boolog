767c8eaac60832be26a362f51f02e87ecab67f3e
natheraf <ma.eric11214@outlook.com>
2024-09-21 19:58:54 -0400

    moved "no reply" name to the end

--- server/mongo/middleware/mailer.js
+++ server/mongo/middleware/mailer.js
@@ -21,7 +21,7 @@ const emailAuthenticationCheck = (req) =>
     });
     transporter
       .sendMail({
-        from: `"Boolog No-Reply 📖" <${config.outgoing_email}>`, // sender address
+        from: `"Boolog 📖 No-Reply" <${config.outgoing_email}>`, // sender address
         to: `${req.body.email}`, // list of receivers
         subject: "Verify Your Identity", // Subject line
         text: `Hi ${
