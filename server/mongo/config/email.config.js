c131af16a08a7db812ee5fd0bf6c581c6ea3acda
natheraf <ma.eric11214@outlook.com>
2024-09-21 01:04:37 -0400

    smtp email config

---
+++ server/mongo/config/email.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  outgoing_email_host: process.env.OUTGOING_EMAIL_HOST,
+  outgoing_email: process.env.OUTGOING_EMAIL,
+  outgoing_email_password: process.env.OUTGOING_EMAIL_PASSWORD,
+  test_incoming_email: process.env.TEST_INCOMING_EMAIL,
+};
