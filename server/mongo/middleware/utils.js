8bba2d9055e407487e6ba5604ef10baf292db790
natheraf <ma.eric11214@outlook.com>
2024-09-21 02:13:51 -0400

    utilities

---
+++ server/mongo/middleware/utils.js
@@ -0,0 +1,21 @@
+const bodyMissingRequiredFields = (req, requiredBody) => {
+  const missing = [];
+  requiredBody.forEach((key) => {
+    if (!req.body[key]) missing.push(key);
+  });
+  if (missing.length > 0) {
+    return {
+      message:
+        (missing.length <= 2
+          ? missing.join(" and ")
+          : missing.slice(0, missing.length - 1).join(", ") +
+            ", and " +
+            missing[missing.length - 1]) + " cannot be empty",
+    };
+  }
+  return false;
+};
+
+module.exports = {
+  bodyMissingRequiredFields,
+};
