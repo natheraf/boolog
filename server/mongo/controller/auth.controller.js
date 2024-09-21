b64a0eee1458a86756b87696a5f9790af3161dc4
natheraf <ma.eric11214@outlook.com>
2024-09-21 02:13:25 -0400

    added passwordless functions

--- server/mongo/controller/auth.controller.js
+++ server/mongo/controller/auth.controller.js
@@ -4,6 +4,9 @@ const superAdminConfig = require("../config/superAdmin.config");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { getDatabase } = require("../database");
+const { isDuplicateEmail } = require("../middleware/verifySignUp");
+const { emailAuthenticationCheck } = require("../middleware/mailer");
+const { bodyMissingRequiredFields } = require("../middleware/utils");
 
 exports.createSuperAdmin = () => {
   getDatabase("authentication")
@@ -13,6 +16,7 @@ exports.createSuperAdmin = () => {
         email: superAdminConfig.email,
         password: bcrypt.hashSync(superAdminConfig.password, 10),
         active: true,
+        role: "super_admin",
       });
     })
     .then(() => {
@@ -25,20 +29,9 @@ exports.createSuperAdmin = () => {
 
 exports.signUp = (req, res, next) => {
   const userRequiredBody = ["name", "email", "password", "role", "active"];
-  const missing = [];
-  userRequiredBody.forEach((key) => {
-    if (!req.body[key]) missing.push(key);
-  });
-  if (missing.length > 0) {
-    res?.status(400).send({
-      message:
-        (missing.length <= 2
-          ? missing.join(" and ")
-          : missing.slice(0, missing.length - 1).join(", ") +
-            ", and " +
-            missing[missing.length - 1]) + " cannot be empty",
-    });
-    return false;
+  const missing = bodyMissingRequiredFields(req, userRequiredBody);
+  if (missing) {
+    return res?.status(400).send(missing);
   }
 
   const user = {};
@@ -57,20 +50,15 @@ exports.signUp = (req, res, next) => {
 };
 
 exports.signIn = (req, res) => {
-  const missing = ["email", "password"].filter((key) => !req.body[key]);
-  if (missing.length > 0) {
-    res.status(400).send({
-      message: "User's " + missing.join(" and ") + " cannot be empty",
-    });
-    return;
+  const missing = bodyMissingRequiredFields(req, ["email"]);
+  if (missing) {
+    return res?.status(400).send(missing);
   }
 
-  console.log(req.body, req.body.email);
   getDatabase("authentication").then((db) => {
     db.collection("loginInfo")
       .findOne({ email: req.body.email })
       .then((user) => {
-        console.log(user);
         if (!user) {
           return res
             .status(401)
@@ -90,14 +78,10 @@ exports.signIn = (req, res) => {
         const expiresIn = "7d"; // 7 days
         res.cookie(
           "accessToken",
-          jwt.sign(
-            { userId: user.id, userRoleId: user.roleId },
-            config.secret,
-            {
-              algorithm: "HS256",
-              expiresIn: expiresIn,
-            }
-          ),
+          jwt.sign({ userId: user._id, userRole: user.role }, config.secret, {
+            algorithm: "HS256",
+            expiresIn: expiresIn,
+          }),
           {
             httpOnly: true,
             secure: true,
@@ -120,5 +104,136 @@ exports.signOut = (req, res) => {
   res.clearCookie("accessToken");
   res.status(200).send({
     message: "User logged out successfully",
+  });
+};
+
+exports.checkPasswordless = (req, res, next) => {
+  const missing = bodyMissingRequiredFields(req, ["email"]);
+  if (missing) {
+    return res?.status(400).send(missing);
+  }
+
+  isDuplicateEmail(req.body.email).then((isDuplicate) => {
+    if (isDuplicate) {
+      getDatabase("authentication").then((db) => {
+        db.collection("loginInfo")
+          .findOne({ email: req.body.email })
+          .then((user) => {
+            if (!user) {
+              return res
+                .status(401)
+                .send({ message: "Email and Password are not found" });
+            }
+            emailAuthenticationCheck(
+              "Verify Your Identity",
+              req.body.email,
+              user.name
+            ).then(({ code, info }) =>
+              db
+                .collection("loginEmailCodes")
+                .insertOne({ email: req.body.email, code, info })
+                .then((insertRes) => {
+                  setTimeout(
+                    () =>
+                      db.collection("loginEmailCodes").deleteOne({
+                        _id: insertRes.insertedId,
+                        email: req.body.email,
+                      }),
+                    1000 * 60 * 5 // 5 minutes
+                  );
+                  res.status.send("Sent verification code");
+                })
+            );
+          });
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+exports.signUpPasswordless = (req, res) => {
+  const missing = bodyMissingRequiredFields(req, ["email", "role", "active"]);
+  if (missing) {
+    return res?.status(400).send(missing);
+  }
+
+  const user = {
+    email: req.body.email,
+    name: req.body.email,
+    password: null,
+    role: req.body.role,
+    active: req.body.active,
+  };
+  getDatabase("authentication")
+    .then((db) => {
+      db.collection("loginInfo")
+        .insertOne(user)
+        .then(() => next());
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+};
+
+exports.signInPasswordless = (req, res) => {
+  const missing = bodyMissingRequiredFields(req, ["email"]);
+  if (missing) {
+    return res?.status(400).send(missing);
+  }
+
+  getDatabase("authentication").then((db) => {
+    db.collection("loginInfo")
+      .findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).send({ message: "User not found" });
+        }
+
+        const expiresIn = "7d"; // 7 days
+        res.cookie(
+          "accessToken",
+          jwt.sign({ userId: user._id, userRole: user.role }, config.secret, {
+            algorithm: "HS256",
+            expiresIn: expiresIn,
+          }),
+          {
+            httpOnly: true,
+            secure: true,
+            signed: true,
+            expiresIn: 60 * 60,
+            sameSite: "Strict",
+          } // expires: 60 * 60 * 24 * 7 // 1 week
+        );
+        res.status(200).send({
+          userName: user.name,
+          userEmail: user.email,
+          message: `Successfully Signed In. Expires in ${expiresIn}.`,
+        });
+      });
+  });
+};
+
+exports.checkPasswordlessCode = (req, res, next) => {
+  const missing = bodyMissingRequiredFields(req, ["email", "verificationCode"]);
+  if (missing) {
+    return res?.status(400).send(missing);
+  }
+
+  getDatabase("authentication").then((db) => {
+    db.collection("loginEmailCodes")
+      .findOne({
+        email: req.body.email,
+        code: req.body.verificationCode,
+      })
+      .then((obj) => {
+        if (!obj) {
+          return res
+            .status(401)
+            .send({ message: "Wrong email and verification code" });
+        }
+        next();
+      });
   });
 };
