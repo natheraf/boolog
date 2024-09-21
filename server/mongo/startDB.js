027048b56e4b1e56c464b05d17d1c245fabe818c
natheraf <ma.eric11214@outlook.com>
2024-09-20 22:59:47 -0400

    added initialization and drop

--- server/mongo/startDB.js
+++ server/mongo/startDB.js
@@ -1,19 +1,33 @@
+const { createSuperAdmin } = require("./controller/auth.controller");
+
 module.exports = (drop) => {
   const { MongoClient } = require("mongodb");
 
   const url = "mongodb://localhost:27017";
   const client = new MongoClient(url);
 
-  async function setUpAuthentication() {
+  async function dropAuthenticationCollections() {
     await client.connect();
-    console.log("Connected successfully to server");
     if (drop) {
       const db = client.db("authentication");
-      const collections = ["loginEmailCodes", "emailAndPasswords"];
+      const collections = ["loginEmailCodes", "loginInfo"];
       collections.forEach((collection) => db.collection(collection).drop());
       console.log("Dropped authentication db collections");
+      db.collection("loginEmailCodes").createIndex(
+        { email: 1 },
+        { unique: true }
+      );
+      db.collection("loginInfo").createIndex({ email: 1 }, { unique: true });
+      console.log("successfully dropped all collections in authentication");
     }
   }
 
-  setUpAuthentication().catch(console.error);
+  const initialize = () => {
+    dropAuthenticationCollections().catch(console.error);
+    createSuperAdmin();
+  };
+
+  if (drop) {
+    initialize();
+  }
 };
