e6379b3c90e4dc36e6c2ad43b9cbc158b1f5ae98
natheraf <ma.eric11214@outlook.com>
2025-01-21 01:48:33 -0500

    test crud for users

--- rclient/src/views/Home.js
+++ rclient/src/views/Home.js
@@ -3,6 +3,13 @@ import { AlertsContext } from "../context/Alerts";
 import { Button } from "@mui/material";
 import { handleSimpleRequest } from "../api/Axios";
 import { getAllBooks } from "../api/IndexedDB/Books";
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getCurrentUser,
+  renameUser,
+} from "../api/IndexedDB/Users";
 
 export const Home = () => {
   const addAlert = React.useContext(AlertsContext).addAlert;
@@ -85,6 +92,36 @@ export const Home = () => {
         }}
       >
         get all settings
+      </Button>
+
+      <br />
+
+      <Button
+        onClick={() => getAllUsers().then((result) => console.log(result))}
+      >
+        get all users
+      </Button>
+      <Button
+        onClick={() => getCurrentUser().then((result) => console.log(result))}
+      >
+        get cur user
+      </Button>
+      <Button
+        onClick={() =>
+          renameUser(1, "foo").then((result) => console.log(result))
+        }
+      >
+        rename user to foo
+      </Button>
+      <Button
+        onClick={() => addUser("foo").then((result) => console.log(result))}
+      >
+        add user foo
+      </Button>
+      <Button
+        onClick={() => deleteUser(5).then((result) => console.log(result))}
+      >
+        delete user
       </Button>
     </div>
   );
