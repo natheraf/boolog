aa8e1d16517d14395ea795980d6166a53fd0b941
natheraf <ma.eric11214@outlook.com>
2025-01-22 00:30:13 -0500

    url query stringify for http methods

--- rclient/src/api/Axios.js
+++ rclient/src/api/Axios.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { getCurrentUserId } from "./IndexedDB/State";
 
 axios.defaults.withCredentials = true;
 const api = `${window.location.protocol}//${window.location.hostname}:4002/api`;
@@ -14,13 +13,25 @@ const httpMethodFunctions = new Map([
  * @param {String} httpMethod GET, POST, DELETE, etc.
  * @param {Object} object passed onto req.body
  * @param {String} route api route appended to root url
+ * @param {Object=} query object map for get url query
  * @returns {Promise} http request promise
  */
-export const handleSimpleRequest = async (httpMethod, object, route) => {
-  object.localUserId = await getCurrentUserId();
+export const handleSimpleRequest = (httpMethod, object, route, query = {}) => {
+  httpMethod = httpMethod.toUpperCase();
+  object.localUserId = localStorage.getItem("userId");
+  query.localUserId = localStorage.getItem("userId");
+  const queryString = `?${Object.entries(query)
+    .map((entry) =>
+      entry
+        .map((value) =>
+          typeof value !== "string" ? JSON.stringify(value) : value
+        )
+        .join("=")
+    )
+    .join("&")}`;
   return new Promise((resolve, reject) =>
     httpMethodFunctions
-      .get(httpMethod.toUpperCase())(`${api}/${route}`, object)
+      .get(httpMethod)(`${api}/${route}${queryString}`, object)
       .then((res) => resolve(res))
       .catch((error) => {
         return reject(Error(error?.response?.data?.message));
