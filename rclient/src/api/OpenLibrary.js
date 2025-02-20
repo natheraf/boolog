f102956410156336886f50de5c01829b53c2b0a7
natheraf <ma.eric11214@outlook.com>
2025-02-20 00:02:53 -0500

    added fields to url query to conform with new api update

--- rclient/src/api/OpenLibrary.js
+++ rclient/src/api/OpenLibrary.js
@@ -1,5 +1,17 @@
 export const searchOpenLib = (query, pageLimit = 5, page = 1) => {
-  const url = `https://openlibrary.org/search.json?${query}&limit=${pageLimit}&page=${page}`;
+  const fields = [
+    "title",
+    "author_name",
+    "publisher",
+    "first_publish_year",
+    "number_of_pages_median",
+    "isbn",
+    "key",
+    "cover_i",
+  ];
+  const url = `https://openlibrary.org/search.json?${query}&limit=${pageLimit}&page=${page}&fields=${fields.join(
+    ","
+  )}`;
   const options = {
     method: "GET",
   };
