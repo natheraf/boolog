8c54283bf0a0e0a8e02d353b52c771732a8a80d9
natheraf <ma.eric11214@outlook.com>
2025-02-22 02:16:39 -0500

    does not off words from preview and tries to cut from spaces. also tries to balance the number of chars

--- rclient/src/features/epubSearch/xPathResultWorker.js
+++ rclient/src/features/epubSearch/xPathResultWorker.js
@@ -1,4 +1,6 @@
-onmessage = ({ data: { text, searchNeedle, spineSearchPointer, page } }) => {
+onmessage = ({
+  data: { text, searchNeedle, spineSearchPointer, page, fragment },
+}) => {
   let index = text.indexOf(searchNeedle);
   const indexAppearances = [];
   while (index !== -1) {
@@ -7,16 +9,34 @@ onmessage = ({ data: { text, searchNeedle, spineSearchPointer, page } }) => {
   }
   const res = [];
   for (const index of indexAppearances) {
+    const previewLength = 130 - searchNeedle.length;
+    const half = Math.floor(previewLength / 2);
+    const startIndex =
+      index - half < 0
+        ? 0
+        : text.indexOf(" ", index - half) >= index
+        ? index - half
+        : text.indexOf(" ", index - half);
+    const unusedStartChars = half - (index - startIndex);
+    let endIndex = Math.min(text.length, index + half + unusedStartChars);
+    while (
+      endIndex < text.length &&
+      endIndex > index + searchNeedle.length &&
+      text[endIndex] !== " "
+    ) {
+      endIndex -= 1;
+    }
+    if (endIndex === index) {
+      endIndex = Math.min(text.length, index + 60 + unusedStartChars);
+    }
     res.push({
       spineIndex: spineSearchPointer,
       page,
       textIndex: index,
-      previewStart: text.substring(index - 50, index),
+      previewStart: text.substring(startIndex, index),
       needle: searchNeedle,
-      previewEnd: text.substring(
-        index + searchNeedle.length,
-        index + searchNeedle.length + 50
-      ),
+      previewEnd: text.substring(index + searchNeedle.length, endIndex),
+      randomKey: Math.floor(Math.random() * 100000),
     });
   }
   postMessage(res);
