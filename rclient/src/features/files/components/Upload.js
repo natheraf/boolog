7efa7df832a54cdc3a123c17c30df85d6969447c
natheraf <ma.eric11214@outlook.com>
2025-02-26 02:40:52 -0500

    added file drag and drop and status

--- rclient/src/features/files/components/Upload.js
+++ rclient/src/features/files/components/Upload.js
@@ -2,13 +2,35 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { useTheme } from "@emotion/react";
 
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, LinearProgress, Stack, Typography } from "@mui/material";
 
-import FileUploadIcon from "@mui/icons-material/FileUpload";
+import DownloadIcon from "@mui/icons-material/Download";
+import BookIcon from "@mui/icons-material/Book";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
 
-export const Upload = ({ file, setFile }) => {
+const LinearProgressWithLabel = (props) => {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {`${Math.round(props.valueBuffer)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export const Upload = ({ file, setFile, originalFile }) => {
   const theme = useTheme();
   const inputFile = React.useRef(null);
+  const [storageEstimate, setStorageEstimate] = React.useState({
+    usage: 0,
+    quota: 0,
+  });
+  const [originalFileSize, setOriginalFileSize] = React.useState(0);
 
   const handleFileChange = (event) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -16,9 +38,54 @@ export const Upload = ({ file, setFile }) => {
     }
   };
 
+  const handleFileClear = () => {
+    setFile(null);
+    inputFile.current.value = null;
+  };
+
   const handleOnClick = () => {
     inputFile.current.click();
   };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    if (
+      event.dataTransfer.items &&
+      event.dataTransfer.items.length > 0 &&
+      event.dataTransfer.items[0].kind === "file"
+    ) {
+      const file = event.dataTransfer.items[0].getAsFile();
+      setFile(file);
+    } else if (
+      event.dataTransfer.files &&
+      event.dataTransfer.files.length > 0
+    ) {
+      const file = event.dataTransfer.files[0];
+      setFile(file);
+    }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const updateStorageEstimate = () => {
+    navigator.storage.estimate().then((res) => setStorageEstimate(res));
+  };
+
+  const scaleStorageUnites = (bytes) => {
+    const scale = ["B", "KB", "MB", "GB", "TB"];
+    let pow = scale.length - 1;
+    while (pow > 0 && bytes / Math.pow(1024, pow) < 1) {
+      pow -= 1;
+    }
+    return `${(bytes / Math.pow(1024, pow)).toFixed(2)} ${scale[pow]}`;
+  };
+
+  React.useEffect(() => {
+    updateStorageEstimate();
+    setOriginalFileSize(originalFile?.size ?? 0);
+  }, []);
 
   return (
     <Stack
@@ -37,26 +104,69 @@ export const Upload = ({ file, setFile }) => {
       }}
       alignItems={"center"}
       justifyItems={"center"}
-      spacing={"10px"}
+      spacing={1}
       color={theme.palette.text.secondary}
       onClick={file ? null : handleOnClick}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
     >
-      <Typography justifySelf={"center"}>
-        {file?.name ?? "Import File"}
-      </Typography>
+      <Stack
+        direction="row"
+        alignItems={"center"}
+        justifyContent={"center"}
+        spacing={1}
+      >
+        {file ? null : <DownloadIcon />}
+        <Typography>{file?.name ?? "Drag a File / Click to Browse"}</Typography>
+        {file ? <BookIcon /> : <AdsClickIcon />}
+      </Stack>
       {file !== null ? (
-        <Button
-          variant="text"
-          color="error"
-          component="label"
-          onClick={() => setFile(null)}
-          size="small"
-        >
-          {"Clear"}
-        </Button>
-      ) : (
-        <FileUploadIcon />
-      )}
+        <Stack spacing={1} alignItems={"center"} justifyItems={"center"}>
+          <Typography>{scaleStorageUnites(file?.size)}</Typography>
+          <Button
+            variant="text"
+            color="error"
+            component="label"
+            onClick={handleFileClear}
+            size="small"
+          >
+            {"Clear"}
+          </Button>
+        </Stack>
+      ) : null}
+      <Typography>
+        {`Usage: 
+        ${scaleStorageUnites(storageEstimate.usage)}`}
+      </Typography>
+      <Typography>
+        {`Total Storage: 
+        ${scaleStorageUnites(storageEstimate.quota / 1000)}`}
+      </Typography>
+      {storageEstimate ? (
+        <LinearProgressWithLabel
+          variant="buffer"
+          value={
+            ((storageEstimate.usage - originalFileSize) /
+              (storageEstimate.quota / 1000)) *
+            100
+          }
+          valueBuffer={
+            ((storageEstimate.usage - originalFileSize + (file?.size ?? 0)) /
+              (storageEstimate.quota / 1000)) *
+            100
+          }
+          sx={{
+            "& .MuiLinearProgress-dashed": {
+              animation: "none",
+              backgroundImage: "none",
+              backgroundColor: "gray",
+            },
+            "& .MuiLinearProgress-bar": {
+              transition: "none",
+            },
+          }}
+        />
+      ) : null}
       <input type="file" ref={inputFile} onChange={handleFileChange} hidden />
     </Stack>
   );
@@ -65,4 +175,5 @@ export const Upload = ({ file, setFile }) => {
 Upload.propTypes = {
   file: PropTypes.object,
   setFile: PropTypes.func.isRequired,
+  originalFile: PropTypes.object,
 };
