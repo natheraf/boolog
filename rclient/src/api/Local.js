0f1d02930b8b5b7d8e302541150634ea8e2c1585
natheraf <ma.eric11214@outlook.com>
2025-02-28 02:22:39 -0500

    moved font families out

--- rclient/src/api/Local.js
+++ rclient/src/api/Local.js
@@ -35,3 +35,32 @@ export const userTemplates = [
       "https://images.unsplash.com/photo-1561731216-c3a4d99437d5?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+
+export const defaultFormatting = {
+  fontSize: 100,
+  _fontSizeStep: 1,
+  _fontSizeBounds: { min: 1, max: Infinity },
+  lineHeight: 12,
+  _lineHeightBounds: { min: 1, max: Infinity },
+  _lineHeightStep: 1,
+  pageMargins: 500,
+  _pageMarginsStep: 50,
+  _pageMarginsBounds: { min: 70, max: Infinity },
+  pagesShown: 1,
+  _pagesShownStep: 1,
+  _pagesShownBounds: { min: 1, max: Infinity },
+  fontFamily: {
+    label: "Original",
+    value: "inherit",
+    group: null,
+    kind: "local",
+  },
+  textAlign: { label: "Original", value: "inherit" },
+  _textAlignments: [
+    { label: "Original", value: "inherit" },
+    { label: "Left", value: "start" },
+    { label: "Middle", value: "center" },
+    { label: "Right", value: "end" },
+    { label: "Justified", value: "justify" },
+  ],
+};
