978cdcdac97978f4d47e7e911a5ac28f8f9a6953
natheraf <ma.eric11214@outlook.com>
2025-03-19 04:30:57 -0400

    fix: xhtml files not rendering

--- rclient/src/features/epub/epubUtils.js
+++ rclient/src/features/epub/epubUtils.js
@@ -65,7 +65,7 @@ export const processEpub = (epubObject) => {
     const parser = new DOMParser();
     const page = parser.parseFromString(
       getEpubValueFromPath(epubObject.html, path),
-      "text/html"
+      type === "xhtml" ? "application/xhtml+xml" : "text/html"
     );
     const nodes = page.querySelectorAll("img, image, a");
     if (!nodes) {
