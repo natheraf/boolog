67163a38861177a1d5758ced968fecbb595ea0f6
natheraf <ma.eric11214@outlook.com>
2026-02-25 17:23:53 -0500

    feat: add SideButtons component for navigation in EPUB reader

---
+++ rclient/src/features/epub/components/SideButtons.js
@@ -0,0 +1,113 @@
+import { Box, Divider, Stack } from "@mui/material";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+export const SideButtons = ({
+  children,
+  leftButtonOnClick,
+  rightButtonOnClick,
+}) => {
+  // temp
+  const [backgroundColors, showDividers, opacityOfSideElements, showArrows] = [
+    "white",
+    true,
+    0.5,
+    true,
+  ];
+
+  return (
+    <Stack
+      direction="row"
+      alignItems={"center"}
+      justifyContent={"center"}
+      sx={{ height: "100%" }}
+      spacing={0}
+      overflow="hidden"
+    >
+      <Box
+        id="previous-page-button"
+        onClick={leftButtonOnClick}
+        sx={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+          cursor: "pointer",
+          zIndex: 1,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backgroundColor: backgroundColors,
+            justifyContent: "flex-end",
+          }}
+        />
+        <Divider
+          orientation="vertical"
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            visibility: showDividers,
+            opacity: opacityOfSideElements,
+          }}
+        />
+        <NavigateBeforeIcon
+          sx={{
+            position: "fixed",
+            left: 0,
+            top: "50%",
+            margin: "auto",
+            visibility: showArrows,
+            opacity: opacityOfSideElements,
+          }}
+          htmlColor={"gray"}
+        />
+      </Box>
+      {children}
+      <Box
+        id="next-page-button"
+        onClick={rightButtonOnClick}
+        sx={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+          cursor: "pointer",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backgroundColor: backgroundColors,
+            justifyContent: "flex-end",
+          }}
+        />
+        <Divider
+          orientation="vertical"
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            visibility: showDividers,
+            opacity: opacityOfSideElements,
+          }}
+        />
+        <NavigateNextIcon
+          sx={{
+            position: "fixed",
+            right: 0,
+            top: "50%",
+            margin: "auto",
+            visibility: showArrows,
+            opacity: opacityOfSideElements,
+          }}
+          htmlColor={"gray"}
+        />
+      </Box>
+    </Stack>
+  );
+};
