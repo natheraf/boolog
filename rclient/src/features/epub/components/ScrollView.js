fe6d3d73395985cae9ae645714a47e092cd4a612
natheraf <ma.eric11214@outlook.com>
2026-02-25 19:27:15 -0500

    feat: implement ScrollView component for enhanced EPUB scrolling and progress management

---
+++ rclient/src/features/epub/components/ScrollView.js
@@ -0,0 +1,118 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
+
+export const ScrollView = ({
+  spine,
+  spineIndex,
+  partProgress,
+  formatting,
+  setProgress,
+}) => {
+  const sentinelsHeight = window.innerHeight;
+
+  const scrollToPercent = (percentage) => {
+    const epubBody = document.getElementById("epub-body");
+    const content = document.getElementById("content");
+    const contentRect = content.getBoundingClientRect();
+    const height = contentRect.height;
+    const targetScrollTop = height * percentage;
+    const includeSentinel = targetScrollTop + sentinelsHeight;
+    if (percentage === 0) {
+      return epubBody.scroll({
+        top: includeSentinel * 0.75,
+      });
+    }
+    if (height > includeSentinel) {
+      return epubBody.scroll({
+        top: includeSentinel,
+      });
+    }
+    const keepScrollAboveBottomSentinel = Math.min(height, includeSentinel);
+    epubBody.scroll({
+      top: keepScrollAboveBottomSentinel,
+    });
+  };
+
+  const timeOutToSetProgress = React.useRef(null);
+  const onScroll = (event) => {
+    clearTimeout(timeOutToSetProgress.current);
+    const content = document.getElementById("content");
+    const contentRect = content.getBoundingClientRect();
+    const goPreviousChapter = event.target.scrollTop <= 5;
+    const goNextChapter =
+      contentRect.height +
+        sentinelsHeight * 2 -
+        (event.target.scrollTop + window.innerHeight) <
+      5;
+
+    if (goPreviousChapter || goNextChapter) {
+      timeOutToSetProgress.current = setTimeout(() => {
+        if (goPreviousChapter) {
+          setProgress(Math.max(0, spineIndex - 1), 0.999999);
+        } else if (goNextChapter) {
+          setProgress(Math.min(spineIndex + 1, spine.length - 1), 0);
+        }
+      }, 100);
+      return;
+    }
+
+    timeOutToSetProgress.current = setTimeout(() => {
+      if (contentRect.top > 0) {
+        setProgress(spineIndex, 0);
+        return;
+      }
+      setProgress(spineIndex, -contentRect.top / contentRect.height);
+    }, 500);
+  };
+
+  React.useEffect(() => {
+    scrollToPercent(partProgress);
+    const epubBody = document.getElementById("epub-body");
+    const resizeObserver = new ResizeObserver(() =>
+      scrollToPercent(partProgress)
+    );
+    resizeObserver.observe(epubBody);
+    setTimeout(() => {
+      epubBody.addEventListener("scroll", onScroll);
+    }, 500);
+    return () => {
+      resizeObserver.disconnect();
+      epubBody.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  return (
+    <Box
+      id="scroll-view"
+      sx={{
+        overflow: "auto",
+        justifyItems: "center",
+        backgroundColor: formatting.backgroundColor,
+      }}
+    >
+      <Box sx={{ height: sentinelsHeight }} />
+      <Box
+        id="content"
+        sx={{
+          minWidth: `${formatting.contentWidth}px`,
+          maxWidth: `${formatting.contentWidth}px`,
+        }}
+        dangerouslySetInnerHTML={{
+          __html:
+            spine?.[spineIndex ?? -1]?.element ??
+            "something went wrong...<br/> spine.current is missing",
+        }}
+      />
+      <Box sx={{ height: sentinelsHeight }} />
+    </Box>
+  );
+};
+
+ScrollView.propType = {
+  spine: PropTypes.array.isRequired,
+  spineIndex: PropTypes.number.isRequired,
+  partProgress: PropTypes.number.isRequired,
+  formatting: PropTypes.object.isRequired,
+  setProgress: PropTypes.func.isRequired,
+};
