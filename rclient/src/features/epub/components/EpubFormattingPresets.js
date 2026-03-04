e5802c2bf3648d2bd54c85402e505b742f516c8f
natheraf <ma.eric11214@outlook.com>
2026-03-04 02:52:13 -0500

    feat: enhance EpubFormattingPresets with tooltip support for toggle buttons

--- rclient/src/features/epub/components/EpubFormattingPresets.js
+++ rclient/src/features/epub/components/EpubFormattingPresets.js
@@ -5,7 +5,7 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 import WbTwilightIcon from "@mui/icons-material/WbTwilight";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import EditIcon from "@mui/icons-material/Edit";
-import { Paper, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Paper, ToggleButton, ToggleButtonGroup, Tooltip } from "@mui/material";
 import { setStateValue } from "../../../api/IndexedDB/State";
 import { getFormattingWithPreset } from "../formattingUtils";
 
@@ -32,21 +32,44 @@ export const EpubFormattingPresets = ({
         onChange={handleOnChange}
         aria-label="color preset"
       >
-        <ToggleButton value="midNight" aria-label="super dark">
-          <NightsStayIcon />
-        </ToggleButton>
-        <ToggleButton value="dark" aria-label="dark">
-          <DarkModeIcon />
-        </ToggleButton>
-        <ToggleButton value="twilight" aria-label="bright">
-          <WbTwilightIcon />
-        </ToggleButton>
-        <ToggleButton value="light" aria-label="super bright">
-          <LightModeIcon />
-        </ToggleButton>
-        <ToggleButton value="custom" aria-label="custom">
-          <EditIcon />
-        </ToggleButton>
+        {[
+          {
+            value: "midNight",
+            ariaLabel: "super dark",
+            title: "Mid Night",
+            icon: NightsStayIcon,
+          },
+          {
+            value: "dark",
+            ariaLabel: "dark",
+            title: "Dark",
+            icon: DarkModeIcon,
+          },
+          {
+            value: "twilight",
+            ariaLabel: "bright",
+            title: "Twilight",
+            icon: WbTwilightIcon,
+          },
+          {
+            value: "light",
+            ariaLabel: "super bright",
+            title: "Light",
+            icon: LightModeIcon,
+          },
+          {
+            value: "custom",
+            ariaLabel: "custom",
+            title: "Custom",
+            icon: EditIcon,
+          },
+        ].map((obj) => (
+          <Tooltip key={obj.value} title={obj.title}>
+            <ToggleButton value={obj.value} aria-label={obj.ariaLabel}>
+              <obj.icon />
+            </ToggleButton>
+          </Tooltip>
+        ))}
       </ToggleButtonGroup>
     </Paper>
   );
