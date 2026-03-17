131d7c98ec54be55d59f954d2b04f613b9f2c457
natheraf <ma.eric11214@outlook.com>
2026-03-17 19:25:11 -0400

    fix: looks

--- rclient/src/features/epub/components/EpubDisplayOptions.js
+++ rclient/src/features/epub/components/EpubDisplayOptions.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { FormControlLabel, Paper, Stack, Switch } from "@mui/material";
+import { Checkbox, FormControlLabel, Paper, Stack } from "@mui/material";
 
 export const EpubDisplayOption = ({ displayOptions, setDisplayOptions }) => {
   const handleCheckedOnChange = (key) => (event) => {
@@ -8,33 +8,33 @@ export const EpubDisplayOption = ({ displayOptions, setDisplayOptions }) => {
   };
 
   return (
-    <Paper sx={{ width: "100%", paddingRight: 3 }}>
+    <Paper component={Stack} sx={{ width: "100%" }} alignItems={"center"}>
       <Stack direction={"column"}>
         <FormControlLabel
-          control={<Switch />}
+          control={<Checkbox />}
           checked={displayOptions.showSpineNavigator}
           onChange={handleCheckedOnChange("showSpineNavigator")}
           label="Chapter Tabs on Left"
           slotProps={{ typography: { variant: "subtitle1" } }}
-          labelPlacement="start"
+          labelPlacement="end"
         />
         {displayOptions.view === "page" && (
           <>
             <FormControlLabel
-              control={<Switch />}
+              control={<Checkbox />}
               checked={displayOptions.showPageNavigator}
               onChange={handleCheckedOnChange("showPageNavigator")}
               label="Page Tabs on Right"
               slotProps={{ typography: { variant: "subtitle1" } }}
-              labelPlacement="start"
+              labelPlacement="end"
             />
             <FormControlLabel
-              control={<Switch />}
+              control={<Checkbox />}
               checked={displayOptions.showDividers}
               onChange={handleCheckedOnChange("showDividers")}
               label="Content Edge"
               slotProps={{ typography: { variant: "subtitle1" } }}
-              labelPlacement="start"
+              labelPlacement="end"
             />
           </>
         )}
