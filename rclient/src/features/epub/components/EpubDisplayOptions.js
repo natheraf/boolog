f4c6d2f80698cb8ce1d26f08a7ca8f3a6bd0e4fb
natheraf <ma.eric11214@outlook.com>
2026-03-17 19:30:17 -0400

    feat: added display option label

--- rclient/src/features/epub/components/EpubDisplayOptions.js
+++ rclient/src/features/epub/components/EpubDisplayOptions.js
@@ -1,5 +1,11 @@
 import PropTypes from "prop-types";
-import { Checkbox, FormControlLabel, Paper, Stack } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 export const EpubDisplayOption = ({ displayOptions, setDisplayOptions }) => {
   const handleCheckedOnChange = (key) => (event) => {
@@ -8,36 +14,39 @@ export const EpubDisplayOption = ({ displayOptions, setDisplayOptions }) => {
   };
 
   return (
-    <Paper component={Stack} sx={{ width: "100%" }} alignItems={"center"}>
-      <Stack direction={"column"}>
-        <FormControlLabel
-          control={<Checkbox />}
-          checked={displayOptions.showSpineNavigator}
-          onChange={handleCheckedOnChange("showSpineNavigator")}
-          label="Chapter Tabs on Left"
-          slotProps={{ typography: { variant: "subtitle1" } }}
-          labelPlacement="end"
-        />
-        {displayOptions.view === "page" && (
-          <>
-            <FormControlLabel
-              control={<Checkbox />}
-              checked={displayOptions.showPageNavigator}
-              onChange={handleCheckedOnChange("showPageNavigator")}
-              label="Page Tabs on Right"
-              slotProps={{ typography: { variant: "subtitle1" } }}
-              labelPlacement="end"
-            />
-            <FormControlLabel
-              control={<Checkbox />}
-              checked={displayOptions.showDividers}
-              onChange={handleCheckedOnChange("showDividers")}
-              label="Content Edge"
-              slotProps={{ typography: { variant: "subtitle1" } }}
-              labelPlacement="end"
-            />
-          </>
-        )}
+    <Paper sx={{ width: "100%", p: 1 }}>
+      <Stack direction={"column"} alignItems={"center"} spacing={1}>
+        <Typography variant="h6">Display Options</Typography>
+        <Stack direction={"column"}>
+          <FormControlLabel
+            control={<Checkbox />}
+            checked={displayOptions.showSpineNavigator}
+            onChange={handleCheckedOnChange("showSpineNavigator")}
+            label="Chapter Tabs on Left"
+            slotProps={{ typography: { variant: "subtitle1" } }}
+            labelPlacement="end"
+          />
+          {displayOptions.view === "page" && (
+            <>
+              <FormControlLabel
+                control={<Checkbox />}
+                checked={displayOptions.showPageNavigator}
+                onChange={handleCheckedOnChange("showPageNavigator")}
+                label="Page Tabs on Right"
+                slotProps={{ typography: { variant: "subtitle1" } }}
+                labelPlacement="end"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                checked={displayOptions.showDividers}
+                onChange={handleCheckedOnChange("showDividers")}
+                label="Content Edge"
+                slotProps={{ typography: { variant: "subtitle1" } }}
+                labelPlacement="end"
+              />
+            </>
+          )}
+        </Stack>
       </Stack>
     </Paper>
   );
